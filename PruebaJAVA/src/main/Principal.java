6d461b78a47e555128b565c2bb59a2aecf2f29b2
Alex Calero Revilla <alexcalerorevilla@gmail.com>
2024-10-04 12:47:09 -0500

    prueba proyecto de Java en netbeans

---
+++ PruebaJAVA/src/main/Principal.java
@@ -0,0 +1,15 @@
+/*
+ * Proyecto : PruebaJAVA
+ * Archivo  : Principal.java
+ * Autor    : Alex Calero Revilla
+ * Codigo   : 20206455
+ * Fecha    : 4 oct. 2024, 12:24:39
+ */
+
+package main;
+
+public class Principal {
+	public static void main(String args[]) {
+		System.out.println("Hola mundo\n");
+	}
+}

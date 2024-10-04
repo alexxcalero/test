8e8a4464d27825b6ac6313032dff19a9066e2e0b
Alex Calero Revilla <alexcalerorevilla@gmail.com>
2024-10-04 12:23:17 -0500

    prueba proyecto de C++ en netbeans

---
+++ PruebaCPP/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * Proyecto : PruebaCPP
+ * Archivo  : main.cpp
+ * Autor    : Alex Calero Revilla
+ * Codigo   : 20206455
+ * Fecha    : 4 de octubre de 2024, 12:20
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+	cout << "Hola mundo\n";
+	return 0;
+}
